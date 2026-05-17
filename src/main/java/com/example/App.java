e92d7b8d70816d7734138320d92da701042c6670
Venus <venustj8@gmail.com>
2026-05-17 13:57:36 +0000

    Complete Kubernetes deployment with Spring Boot

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD Pipeline!");
+        SpringApplication.run(App.class,args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Jenkins + Docker + Kubernetes CI/CD Pipeline!";
     }
 }
